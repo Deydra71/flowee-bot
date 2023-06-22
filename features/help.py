d0f857446df83a89462bcc0ad6aa17d00121c008
Veronika Fisarova <100305993+Deydra71@users.noreply.github.com>
2023-06-22 09:43:53 +0200

    Update help.py

--- features/help.py
+++ features/help.py
@@ -26,6 +26,8 @@ class Help(commands.Cog):
         \t\t gratitude, wisdom, love, patience, courage, comfort, faith, joy, hope, serenity, purity.
         \t:bulb: Example: `!prayer guidance`.
 
+        :earth_africa: **Additional Resources**: If you are looking for recources for your Walk in Christ, comman me with `!resources`, and I will print them for you!  :white_heart:
+
         :trophy: **Bible Trivia Games**: I orchestrate Bible Trivia games! To start a game, just type `!trivia` followed by the level (1, 2, or 3) and the players' @usernames. 
         \t:bulb: Example: `!trivia 1 @user1 @user2`.
         \t:grey_exclamation: Please note that you can only start a Bible Trivia game in a dedicated channel!
